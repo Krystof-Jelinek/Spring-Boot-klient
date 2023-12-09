d938fa746f73927366d00493d3b38603ec494839
Krystof Jelinek <krys.jelinek@seznam.cz>
2023-12-09 19:04:35 +0100

    Initial commit

---
+++ src/main/java/cz/fit/cvut/tjv/klient/tjv_klient/TjvKlientApplication.java
@@ -0,0 +1,13 @@
+package cz.fit.cvut.tjv.klient.tjv_klient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjvKlientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjvKlientApplication.class, args);
+	}
+
+}
